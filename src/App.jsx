1486523a9781de773d23ae9a6745c5debd36ae59
Ozmacun <149619575+Ozmacun@users.noreply.github.com>
2025-02-10 23:25:38 +0300

    Added HomePage component and integrate slider functionality; removed unused layout components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-
-import PageContent from "./layout/PageContent";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PageContent/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Diğer route'larınızı buraya ekleyebilirsiniz */}
+      </Routes>
+    </Router>
   );
 }
 
